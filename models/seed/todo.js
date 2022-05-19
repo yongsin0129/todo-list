f52a86ca46da11709309cfdeb8cf694c9dfee499
yongsin0129 <yongsin0129@gmail.com>
2022-05-19 11:09:47 +0800

    feat: add mongoose

---
+++ models/seed/todo.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const todoSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  done: Boolean
+})
+module.export = mongoose.model('Todo', todoSchema)
