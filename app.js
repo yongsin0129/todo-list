6a194b01616384d399a0c91031da8a2e428d043b
yongsin0129 <yongsin0129@gmail.com>
2022-05-19 12:13:04 +0800

    feat: add handlebars

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const exphbs = require('express-handlebars').engine
 const app = express()
 const port = 3000
+
+app.engine('hbs', exphbs({ extname: 'hbs' }))
+app.set('view engine', 'hbs')
 
 mongoose.connect(process.env.MONGODB_URI)
 const db = mongoose.connection
@@ -14,7 +18,7 @@ db.once('open', () => {
 })
 
 app.get('/', (req, res) => {
-  res.send('hello world')
+  res.render('index')
 })
 
 app.listen(port, () => {
