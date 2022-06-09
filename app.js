256bcdd71f33e1c1cac4b5a2c61ce35cde940835
ysRedhouse <yongsin0129@gmail.com>
2022-06-09 16:13:41 +0800

    feat: add message partials

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const methodOverride = require('method-override')
 const router = require('./routes')
 require('./config/mongoose')
 const usePassport = require('./config/passport')
+const flash = require('connect-flash')
 const app = express()
 // 如果在 Heroku 環境則使用 process.env.PORT
 // 否則為本地環境，使用 3000
@@ -27,11 +28,13 @@ app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
 usePassport(app)
-
+app.use(flash())
 app.use((req, res, next) => {
   // 你可以在這裡 console.log(req.user) 等資訊來觀察
   res.locals.isAuthenticated = req.isAuthenticated()
   res.locals.user = req.user
+  res.locals.success_msg = req.flash('success_msg') // 設定 success_msg 訊息
+  res.locals.warning_msg = req.flash('warning_msg') // 設定 warning_msg 訊息
   next()
 })
 // 將 request 導入路由器
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -22,13 +22,30 @@ router.get('/register', (req, res) => {
 router.post('/register', (req, res) => {
   // 取得註冊表單參數
   const { name, email, password, confirmPassword } = req.body
+  const errors = []
+  if (!name || !email || !password || !confirmPassword) {
+    errors.push({ message: '所有欄位都是必填。' })
+  }
+  if (password !== confirmPassword) {
+    errors.push({ message: '密碼與確認密碼不相符！' })
+  }
+  if (errors.length) {
+    return res.render('register', {
+      errors,
+      name,
+      email,
+      password,
+      confirmPassword
+    })
+  }
   // 檢查使用者是否已經註冊
   User.findOne({ email })
     .then(user => {
       // 如果已經註冊：退回原本畫面
       if (user) {
-        console.log('User already exists.')
+        errors.push({ message: '這個 Email 已經註冊過了。' })
         res.render('register', {
+          errors,
           name,
           email,
           password,
@@ -41,7 +58,10 @@ router.post('/register', (req, res) => {
           email,
           password
         })
-          .then(() => res.redirect('/'))
+          .then(() => {
+            req.flash('success_msg', '你已經成功註冊。')
+            res.redirect('/users/login')
+          })
           .catch(err => console.log(err))
       }
     })
@@ -50,6 +70,7 @@ router.post('/register', (req, res) => {
 
 router.get('/logout', (req, res) => {
   req.logout()
+  req.flash('success_msg', '你已經成功登出。')
   res.redirect('/users/login')
 })
 
